c16e5a70b3274674755c74b473eac100a8642974
aman482006-beep <aman482006@gmail.com>
2026-02-12 04:32:53 +0530

    Fix card background cross-browser

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/yash-kalra-portfolio/'
+
+  base: '/yash-kalra-portfolio/',
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
